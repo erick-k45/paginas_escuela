eb5c69c912f5e81bb66f07707fdcbcbef04c5feb
erick-k45 <erick.redhawk@gmail.com>
2019-06-19 19:18:11 -0500

    Add files via upload

---
+++ ergonomia/ergonomia.php
@@ -0,0 +1,63 @@
+<html>
+
+<head>
+<meta http-equiv="Content-Language" content="es">
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<title>Postura correcta</title>
+</head>
+
+<body background="image/f2.png">
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<div align="center">
+	<table border="1" width="72%" background="image/324.jpg">
+		<tr>
+			<td colspan="3">
+			<img border="0" src="image/e.jpg" width="100%" height="205"></td>
+		</tr>
+		<tr>
+			<td width="99%" colspan="3">
+<div class="container">
+			<center>
+				<h1>¿Qué es la Ergonomía?</h1>
+							<p>&nbsp;</p>
+						
+							<img border="0" src="image/guia-ergonomia.jpg" width="323" height="171">
+							
+								<p align="justify">
+							<span lang="ES" style="font-family: Arial,sans-serif">
+							Se denomina ergonomía al estudio del trabajo en 
+							relación con el entorno en que se lleva a cabo (el 
+							lugar de trabajo) y con quienes lo practiquen 
+							(trabajadores). Se utiliza para determinar cómo 
+							diseñar o adaptar el lugar de trabajo al trabajador 
+							con la finalidad de evitar distintos problemas de 
+							salud&nbsp; y aumentar la eficiencia.</span>
+							<p>&nbsp;</p>
+							<p>&nbsp;</p>
+							
+							<span lang="ES" style="font-family: Arial,sans-serif">
+							La aplicación de la ergonomía al lugar de trabajo 
+							reporta muchos beneficios evidentes; para el 
+							trabajador, unas condiciones laborales más sanas y 
+							seguras; para el empleador, el beneficio más patente 
+							es un aumento de la productividad.</span></p>
+							&nbsp;</p>
+							<br>
+						</center>
+				</div>
+				</div>
+			<div style="position: absolute; width: 100px; height: 68px; z-index: 3; left: 5px; top: 59px" id="capa6">
+				<a href="Inicio.php">
+				<img border="0" src="image/regresar_btn.png" width="172" height="70"></a></div>
+			</td>
+		</tr>
+		<tr>
+			<td width="99%"><?php include('pie.php'); ?></td>
+		
+		</tr>
+	</table>
+</div>
+
+</body>
+
+</html>
---
+++ ergonomia/ihc 2/cvista.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang="es"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js js" lang="es"><!--<![endif]--><head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title>Salud e inform&aacute;tica</title>
+		
+		
+
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		 <meta name="viewport" content="width=device-width, initial-scale=1">
+		
+       
+		<style type="text/css" media="all">
+@import url("css/main.css");
+@import url("css/style.css");
+</style>
+
+
+</head>
+
+	<body class="html front not-logged-in no-sidebars page-node page-node- page-node-1 node-type-seccion">
+		
+<?php include('header.php'); ?>
+
+<!-- //// MAIN \\\\ -->
+<main style="padding-top: 0px;" class="grid">
+
+
+
+  
+
+    <!-- //// CONTENT \\\\ -->
+<div class="cell-860">
+			
+
+<center>
+				
+						
+							<h1>CUIDANDO TU VISTA</h1>
+							<img border="0" src="image/v1.jpg" width="323" height="171">
+								<p align="justify">
+							
+							Ls siguientes consejos te ayudaran a cuidar de ti y de tu vista 
+							al estar frente al computador o el móvil  </p>
+							<br>
+							
+
+							<h2>Haz descansos cada cierto tiempo</h2>
+							<img border="0" src="image/v3.jpg" width="323" height="171">
+							<br>
+							<p align="justify">
+							 Por ejemplo, una pausa de 5 minutos cada hora.</p>
+							<br>
+							
+							
+							<h2>Para descansar los ojos tápalos con las manos y ábrelos para mirar a la oscuridad</h2>
+							<img border="0" src="image/v2.jpg" width="382" height="200">
+							<br>
+							<p align="justify">
+							Esto relaja más la vista que simplemente cerrarlos. Otro ejercicio que puedes utilizar es alternar la vista entre un objeto cercano y uno lejano.</p>
+							&nbsp;
+
+							<h2>Establece el brillo del monitor al mínimo posible con el que te encuentres cómodo.</h2>
+							<img border="0" src="image/v4.gif" width="323" height="171">
+							<br>
+							<br>
+							
+
+							<h2>Asegúrate de que hay suficiente luz.</h2>
+							<img border="0" src="image/v5.jpg" width="323" height="171">
+							<br>
+							<p align="justify">
+							 Mucho mejor si es natural. Eso si, ten en cuenta que el exceso de luz también puede ser perjudicial.</p>
+							<br>
+							
+
+
+						</center>
+
+
+
+
+
+
+  </div>
+                <!-- //// ASIDE \\\\ -->
+  <aside class="cell-320">
+                                            
+
+	<section class="block-comparador" style="margin-bottom: 30px;">
+            <section class="nav-header">
+                <div>
+                    <div class="nav-text">
+                        <p>&nbsp;P&aacute;ginas relacionadas &nbsp;&nbsp;&nbsp;&nbsp;</p>
+                    </div>
+                    <div class="nav-text-2">
+                        <p><a href="https://www.nlm.nih.gov/medlineplus/spanish/ergonomics.html">MEDLINE PLUS</a><br>
+
+                        <a href="http://saludocupacionalergonomiaudea.blogspot.mx/p/normal-0-21-false-false-false-es-co-x.html">  SALUD Y ERGONOM&Iacute;A</a><br></p>
+                    </div>
+                </div>
+            </section>
+</section>
+              
+                    </aside>
+                                <!-- //// FIN ASIDE \\\\ -->
+            
+        <!-- //// col-full-1-fin \\\\ -->    <!-- //// FIN CONTENT \\\\ -->
+        
+    
+   
+</main>
+<!-- //// FIN MAIN \\\\ -->
+<!-- //// FOOTER \\\\ -->
+<?php include('pie.php'); ?>
+
+
+
+</body></html>
+
+
+
+
+				
+	
